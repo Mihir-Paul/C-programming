774fa114ae37e7fd3e7549b38c6ddc275c4abb7c
Mihir-Paul <mihirpgm@gmail.com>
2026-09-18 16:45:24 +0530

    Add vardeclare.c with variable declarations and basic arithmetic operations

---
+++ Instructions and Operators/vardeclare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a =22;
+    int b = a;
+    int c = b*5;
+    int d = 1,e;
+
+    int oldage = 24;
+    int years = 2;
+    int new_age = oldage + years;
+    
+    printf ("%d \n%d \n%d \n%d",a,b,c,d);
+}
