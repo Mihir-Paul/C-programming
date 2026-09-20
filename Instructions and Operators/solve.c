65ef3417d5b6efdff92c6ffd1aacbc7828533131
Mihir-Paul <mihirpgm@gmail.com>
2026-09-20 16:04:09 +0530

    Add initial implementation of arithmetic operations in solve.c

---
+++ Instructions and Operators/solve.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a = 4+9*10;
+    int b = 4*3/6*6;
+    int c = 5*2-2*3;
+    int d = 5*(2/2)*3;
+    int e = 5+2/2*3;
+    printf("%d \n%d \n%d \n%d \n%d",a,b,c,d,e);
+}
