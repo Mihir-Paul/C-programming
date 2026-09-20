71889d2aad75aab85bc81bcd2a8f9f3a29961713
Mihir-Paul <mihirpgm@gmail.com>
2026-09-20 23:28:44 +0530

    Add implementation for marks classification in marks.c

--- Conditional Statements/marks.c
+++ Conditional Statements/marks.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    
+    int marks;
+    printf("Enter the marks:");
+    scanf("%d",&marks);
+    if (marks>0 && marks <=30){
+        printf("Failed!");
+    }
+    else if(marks<=100 && marks>30) {
+        printf("Passed!");
+    }
+    else {
+        printf("Invalid Marks");
+    }
+
 }
