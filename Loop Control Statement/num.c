5aa79c95b56946ee82bc0763a36d98cf4e59ffeb
Mihir-Paul <mihirpgm@gmail.com>
2026-09-21 13:40:57 +0530

    Add initial implementations of loop control statements in helloworld.c, num.c, and plus-plus.c

---
+++ Loop Control Statement/num.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int num = 1;
+    for(num;num<=100;num+=1){
+        printf("%d\n",num);
+    }
+}
---
+++ Loop Control Statement/plus-plus.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int i =1;
+    printf("%d\n",i++); //first initiate,then increase
+    printf("%d\n",i);
+
+    printf("%d\n",++i); //first increase
+    printf("%d\n",i);
+}
